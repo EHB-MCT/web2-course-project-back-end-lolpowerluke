1fc5cde2670aa9958dc482a0599728b66180584b
lolpowerluke <lukas.van.heddegem@gmail.com>
2025-11-18 15:06:31 +0100

    start dewm endpoint - returns 200 if given id exists - returns 404 if given id doesnt exist

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config()
+
+let env = process.env;
+let uri = `mongodb+srv://${env.database_login_name}:${env.database_password}@webii.44euncl.mongodb.net/?appName=WEBII`
 
 const app = express();
 const port = 3000;
@@ -7,49 +14,71 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const database = client.db("project");
+
 // Get
-app.get("/user", (req, res) => {
+app.get("/api/user", (req, res) => {
   let ID = req.query.id
 }) 
 
-app.get("/distro", (req, res) => {
+app.get("/api/distro", (req, res) => {
   let ID = req.query.id
 }) 
 
-app.get("/dewm", (req, res) => {
-  let ID = req.query.id
+app.get("/api/dewm", async (req, res) => {
+  let findResult;
+  let ID = req.query.id;
+  const collection = database.collection("dewm");
+  findResult = await collection.findOne({id: parseInt(ID)});
+  if (findResult != null) {
+    res.status(200).json({
+      "dewm": findResult,
+    })
+  } else {
+    res.status(404).json({
+      "message": `No DE/WM found with id ${ID}`,
+    })
+  }
 }) 
 
-app.get("/tests", (req, res) => {
+app.get("/api/tests", (req, res) => {
   let ID = req.query.id
   let TYPE = req.query.type
 }) 
 
-app.get("/quiz", (req, res) => {
+app.get("/api/quiz", (req, res) => {
   let TYPE = req.query.type
 }) 
 
 // Post
-app.post("/quiz", (req, res) => {
+app.post("/api/quiz", (req, res) => {
   let data = req.body;
 }) 
 
-app.post("/user", (req, res) => {
+app.post("/api/user", (req, res) => {
   let data = req.body;
 }) 
 
 // Update
-app.put("/user", (req, res) => {
+app.put("/api/user", (req, res) => {
   let ID = req.query.id
   let data = req.body;
 }) 
 
 // Delete
-app.delete("/user", (req, res) => {
+app.delete("/api/user", (req, res) => {
   let ID = req.query.id
 }) 
 
-app.delete("/test", (req, res) => {
+app.delete("/api/test", (req, res) => {
   let ID = req.query.id
 }) 
 
