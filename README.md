1609f6d5f24ff6d3723a91588901516bcc2c5232
lolpowerluke <lukas.van.heddegem@gmail.com>
2025-11-23 20:48:40 +0100

    populate all get requests also added async to all endpoints /api/tests endpoints has to be tested later to make sure it works

--- README.md
+++ README.md
@@ -11,12 +11,5 @@ This should contain step by step instructions to run your project (delete this s
 Also include login credentials or other vital information if needed.
 
 ## Sources 🗃️
-List **ALL your used sources** here:
-- Used a tutorial or example code? Place a link to them here. (youtube, website, github, ...) and **list the files in which they were used**.
-- Used ChatGPT? Generate a ChatGPT share link: options (three dots) > share and **list the files in which it was used**. This will not work if you have uploaded images in your prompt. In that case, add screenshots instead.
+- [Used to get the "correct" status code for a missing parameter.](https://stackoverflow.com/a/10323055)
 
-It's not enough to only list the sources here, also add the sources to the top of the files in which you used them and indicate the specific parts of the code which are not your own (by writing comments and refering to the correct source(s)). \
-IF you use a source as the base/start for a file, then first place the source in the file and **COMMIT it IMMEDIATELY, BEFORE** adding your own code or modifying it. \
-**ALWAYS** use concise BUT CLEAR commit messages!
-
-Have a look at the example repository: https://github.com/EHB-MCT/example-readme
--- server.js
+++ server.js
@@ -25,72 +25,157 @@ const client = new MongoClient(uri, {
 const database = client.db("project");
 
 // Get
-app.get("/api/user", (req, res) => {
-  let ID = req.query.id
+app.get("/api/user", async (req, res) => {
+  let findResult;
+  let ID = req.query.id;
+  if (ID == undefined) {
+    res.status(422).json({
+      "message": `No user ID found in request!`,
+    })
+  } else {
+    const collection = database.collection("users");
+    findResult = await collection.findOne({id: parseInt(ID)}, {projection: {_id: 0}});
+    if (findResult != null) {
+      res.status(200).json({
+        "user": findResult,
+      })
+    } else {
+      res.status(404).json({
+        "message": `No user found with id ${ID}`,
+      })
+    }
+  }
 }) 
 
 app.get("/api/distro", async (req, res) => {
   let findResult;
   let ID = req.query.id;
-  const collection = database.collection("distros");
-  findResult = await collection.findOne({id: parseInt(ID)});
-  if (findResult != null) {
-    res.status(200).json({
-      "dewm": findResult,
+  if (ID == undefined) {
+    res.status(422).json({
+      "message": `No distro ID found in request!`,
     })
   } else {
-    res.status(404).json({
-      "message": `No distro found with id ${ID}`,
-    })
+    const collection = database.collection("distros");
+    findResult = await collection.findOne({id: parseInt(ID)}, {projection: {_id: 0}});
+    if (findResult != null) {
+      res.status(200).json({
+        "distro": findResult,
+      })
+    } else {
+      res.status(404).json({
+        "message": `No distro found with id ${ID}`,
+      })
+    }
   }
 }) 
 
 app.get("/api/dewm", async (req, res) => {
   let findResult;
   let ID = req.query.id;
-  const collection = database.collection("dewm");
-  findResult = await collection.findOne({id: parseInt(ID)});
-  if (findResult != null) {
-    res.status(200).json({
-      "dewm": findResult,
+  if (ID == undefined) {
+    res.status(422).json({
+      "message": `No DE/WM ID found in request!`,
     })
   } else {
-    res.status(404).json({
-      "message": `No DE/WM found with id ${ID}`,
-    })
+    const collection = database.collection("dewm");
+    findResult = await collection.findOne({id: parseInt(ID)}, {projection: {_id: 0}});
+    if (findResult != null) {
+      res.status(200).json({
+        "dewm": findResult,
+      })
+    } else {
+      res.status(404).json({
+        "message": `No DE/WM found with id ${ID}`,
+      })
+    }
   }
 }) 
 
-app.get("/api/tests", (req, res) => {
-  let ID = req.query.id
-  let TYPE = req.query.type
+// has yet to be fully tested since I was too lazy to add example data to the database
+app.get("/api/tests", async (req, res) => {
+  let findResult;
+  let ID = req.query.id;
+  let TYPE = req.query.type;
+  if (ID == undefined) {
+    res.status(422).json({
+      "message": `No user ID found in request!`,
+    })
+  } else if (TYPE == undefined) {
+    res.status(422).json({
+      "message": `No test type found in request!`,
+    })
+  } else if (TYPE.toLowerCase() != "dewm" && TYPE.toLowerCase() != "distro") {
+    res.status(404).json({
+      "message": `Given test type doesn't exist!`,
+    })
+  } else {
+    const collection = database.collection("tests");
+    let query = {id: parseInt(ID), type: TYPE.toLowerCase()}
+    findResult = collection.find(query).project({_id: 0});
+    if ((await collection.countDocuments(query)) === 0) {
+      res.status(404).json({
+        "message": `No tests found with user id ${ID} and type ${TYPE}`,
+      })
+    } else {
+      res.status(200).json({
+        "test": findResult,
+      })
+    }
+  }
 }) 
 
-app.get("/api/quiz", (req, res) => {
-  let TYPE = req.query.type
+app.get("/api/quiz", async (req, res) => {
+  let findResult;
+  let NUMBER = req.query.number;
+  let TYPE = req.query.type;
+  if (NUMBER == undefined) {
+    res.status(422).json({
+      "message": `No question NUMBER found in request!`,
+    })
+  } else if (TYPE == undefined) {
+    res.status(422).json({
+      "message": `No test type found in request!`,
+    })
+  } else if (TYPE.toLowerCase() != "dewm" && TYPE.toLowerCase() != "distro") {
+    res.status(404).json({
+      "message": `Given test type doesn't exist!`,
+    })
+  } else {
+    const collection = database.collection(TYPE.toLowerCase() + "_test");
+    findResult = await collection.findOne({question_number: parseInt(NUMBER)}, {projection: {_id: 0}});
+    if (findResult != null) {
+      res.status(200).json({
+        "question": findResult,
+      })
+    } else {
+      res.status(404).json({
+        "message": `No question found with number ${NUMBER}`,
+      })
+    }
+  }
 }) 
 
 // Post
-app.post("/api/quiz", (req, res) => {
+app.post("/api/quiz", async (req, res) => {
   let data = req.body;
 }) 
 
-app.post("/api/user", (req, res) => {
+app.post("/api/user", async (req, res) => {
   let data = req.body;
 }) 
 
 // Update
-app.put("/api/user", (req, res) => {
+app.put("/api/user", async (req, res) => {
   let ID = req.query.id
   let data = req.body;
 }) 
 
 // Delete
-app.delete("/api/user", (req, res) => {
+app.delete("/api/user", async (req, res) => {
   let ID = req.query.id
 }) 
 
-app.delete("/api/test", (req, res) => {
+app.delete("/api/test", async (req, res) => {
   let ID = req.query.id
 }) 
 
